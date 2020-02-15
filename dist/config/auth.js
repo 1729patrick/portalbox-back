b24311bcb50298e1facce40504e6152b9458ed15
Patrick Battisti Forsthofer <190200007@estudantes.ips.pt>
2020-02-15 14:21:02 +0000

    Upgrade package

--- dist/config/auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = {
-  secret: process.env.APP_SECRET,
-  expiresInCompany: '7d',
-  expiresInVisitor: '1d',
-};
